0eaa7044006f8bfd199a319cacb4450de514f78f
nerrevar <nerrevar12@yandex.ru>
2020-05-31 23:43:30 +0300

    kv_gorup migrations

--- claim/admin.py
+++ claim/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Question, KV, Claim
+from .models import Question, KV, Claim, Group
 
 # Register your models here.
 
 admin.site.register(Question)
 admin.site.register(KV)
 admin.site.register(Claim)
+admin.site.register(Group)
--- claim/models.py
+++ claim/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    group_name = models.CharField(max_length = 80, unique = True, default = 'default')
+
 class Question(models.Model):
-    question_number = models.PositiveSmallIntegerField()
+    question_number = models.PositiveSmallIntegerField(unique = True)
     question_text = models.CharField(max_length = 100)
 
 class KV(models.Model):
     KV_login = models.CharField(max_length = 30)
-    KV_name = models.CharField(max_length = 80)
+    KV_name = models.CharField(max_length = 80, unique = True)
+    group_name = models.ForeignKey('Group', on_delete = models.CASCADE, to_field = 'group_name', default = 'default')
 
 class Claim(models.Model):
-    KV_name = models.ForeignKey('KV', on_delete = models.CASCADE)
-    question_number = models.ForeignKey('Question', on_delete = models.CASCADE)
+    KV_name = models.ForeignKey('KV', on_delete = models.CASCADE, to_field = 'KV_name')
+    question_number = models.ForeignKey('Question', on_delete = models.CASCADE, to_field = 'question_number')
     error_date = models.DateField(auto_now=True)
