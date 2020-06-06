288b0e369bdc37cd9a2633d3f7b403940774d6bc
nerrevar <nerrevar12@yandex.ru>
2020-06-06 18:48:27 +0300

    fixed jquery include, change add_error fields, added stat_kv and stat_questions pages

--- claim/urls.py
+++ claim/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('add_error', views.add_error, name='add_error'),
     path('', views.stat, name='stat'),
-    path('write_error', views.write_error, name='write_error')
-    # path('stat_kv', views.stat_kv, name='stat_kv')
+    path('write_error', views.write_error, name='write_error'),
+    path('stat_kv', views.stat_kv, name='stat_kv'),
+    path('stat_question', views.stat_question, name='stat_question')
 ]
--- claim/views.py
+++ claim/views.py
@@ -21,68 +21,53 @@ def add_error(request):
 
 def stat(request):
     template = loader.get_template('claim/stat.html')
-    question = Question.objects.order_by('question_number')
-    claim_arr = Claim.objects.all()
-    count_arr = dict.fromkeys([group.group_name for group in Group.objects.all()])
-    for group in Group.objects.all():
-        count_arr[group.group_name] = dict.fromkeys([kv.KV_name for kv in KV.objects.all().filter(group_name=group.group_name)])
-        kv_err_count = 0
-        for kv in KV.objects.filter(group_name=group.group_name):
-            kv_err_count += len(Claim.objects.filter(KV_name=kv.KV_name))
-        count_arr[group.group_name]['error_count'] = kv_err_count # Group error count
-
-        kv = KV.objects.filter(group_name = group.group_name) # KV from current group
-        for k in kv:
-            count_arr[group.group_name][k.KV_name] = list()
-            count_arr[group.group_name][k.KV_name].append(k.KV_name)
-            count_arr[group.group_name][k.KV_name].append(k.KV_login)
-            for q in question:
-                error_count = len(claim_arr.filter(question_number=q.question_number, KV_name=k.KV_name))
-                count_arr[group.group_name][k.KV_name].append(error_count) # Add error count for each question to KV's list
-            count_arr[group.group_name][k.KV_name].append(len(claim_arr.filter(KV_name=k.KV_name))) # Add summary error count
-
-    summary_arr = list()
-    summary_arr.append('')
-    summary_arr.append('Итого')
-    for q in question:
-        summary_arr.append(len(claim_arr.filter(question_number=q.question_number)))
     context = {
         'title': 'Статистика',
         'claim_len': len(Claim.objects.all()),
-        'Group': Group.objects.all(),
-        'KV': KV.objects.all(),
+        'Group': Group.objects.order_by('group_name'),
+        'KV': KV.objects.order_by('KV_name'),
         'Question': Question.objects.order_by('question_number')
     }
-    print(count_arr)
     return HttpResponse(template.render(context, request))
 
-# def stat_kv(request):
-#     template = loader.get_template('claim/stat_kv.html')
-#     claim_arr = Claim.objects.all()
-#     kv = KV.objects.order_by('KV_login')
-#     kv_list = list();
-#     for k in kv:
-#         kv_list.append(k.KV_name)
-#     count_arr = dict.fromkeys(kv_list)
-#     for k in kv:
-#         count_arr[k.KV_name] = len(claim_arr.filter(KV_name=k.id))
-#     sorted_arr = {k: v for k, v in sorted(count_arr.items(), key=lambda item: item[1])}
-#     context = {
-#         'title': 'Статистика КВ',
-#         'count_arr': sorted_arr
-#     }
-#     return HttpResponse(template.render(context, request))
+def stat_kv(request):
+    template = loader.get_template('claim/stat_kv.html')
+    error_count_arr = {kv.KV_name: kv.Error_summary for kv in KV.objects.all()}
+    sorted_arr = {k: error_count_arr[k] for k in sorted(error_count_arr, key=error_count_arr.get, reverse=True)}
+    count_arr = list()
+    for key, value in sorted_arr.items():
+        kv = KV.objects.get(KV_name=key)
+        count_arr.append([kv.KV_name, kv.KV_login, value])
+    context = {
+        'title': 'Статистика по КВ',
+        'count_arr': count_arr
+    }
+    return HttpResponse(template.render(context, request))
+
+def stat_question(request):
+    template = loader.get_template('claim/stat_question.html')
+    error_count_arr = {'{0}. {1}'.format(q.question_number, q.question_text): q.Count for q in Question.objects.all()}
+    sorted_arr = {k: error_count_arr[k] for k in sorted(error_count_arr, key=error_count_arr.get, reverse=True)}
+    context = {
+        'title': 'Статистика по вопросам',
+        'count_arr': sorted_arr
+    }
+    return HttpResponse(template.render(context, request))
 
 @csrf_exempt
 def write_error(request):
     data = request.POST
-    if data.get('kv_id') and data.get('q_id'):
-        if data.get('kv_id') != 'None':
-            if data.get('q_id') != 'None':
+    try:
+        kv = KV.objects.get( KV_name=data.get('kv_name') )
+        q = Question.objects.get( question_number=data.get('question').split('.')[0] )
+        if kv:
+            if q:
                 c = Claim(
-                    KV_name=KV.objects.get(KV_name=data.get('kv_id')),
-                    question_number=Question.objects.get(question_number=data.get('q_id'))
+                    KV_name=kv,
+                    question_number=q
                 )
                 result = c.save()
                 return HttpResponse(result)
-    return HttpResponse('false')
+        return HttpResponse('except kv or q')
+    except:
+        return HttpResponse('false')
